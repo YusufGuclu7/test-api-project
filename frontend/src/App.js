54a435739baafa6035850b4df2dabf04276e5b2e
YusufGuclu7 <yusufguclu99@hotmail.com>
2025-09-13 05:44:33 +0300

    Fix: Re-added frontend as a regular folder

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DataTable from './DataTable';
+
+function App() {
+  return (
+    <div style={{ padding: 32 }}>
+      <h1>Test API Verileri</h1>
+      <DataTable />
+    </div>
+  );
+}
+
+export default App;
